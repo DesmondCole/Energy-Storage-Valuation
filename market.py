7d2f1e3a91bd90e36972e8ebc8021c24544f8a4b
Desmond Cole <desmond.r.cole@gmail.com>
2019-01-21 23:03:56 -0700

    data and market updates

--- market.py
+++ market.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
-import sklearn as skl
+import monte_carlo
+from sklearn import linear_model as lm
 
 class MarketSimulator:
-    def __init__(self, *, prices):
-        sim_prices = self._do_prices(prices = prices, sims = nsims)
-        return sim_prices
+    def __call__(self, *, testdata):
+        runprices = self._calibration(data = testdata)
+        results = runprices
+        return results
 
     def _do_prices(self, *, prices, sims):
-        input_rels = self._calibration(data = prices)
-        sim_inputs = self._montecarlo(n = sims)
-        results = np.matmul(inputs = sim_inputs, coefs = input_rels)
+        input_rels = self._calibration(data=prices)
+        sim_inputs = monte_carlo.mc_gen(n=sims, datalength=len(prices))
+        results = np.matmul(inputs=sim_inputs, coefs=input_rels)
         return results
 
     def _calibration(self, *, data):
-        Y = data[, 0]
-        X = data[, 1:]
-        linmod = skl.LassoCV(Y, X)
+        Y = data.iloc[:, 0]
+        X = data.iloc[:, 1:]
+        linmod = lm.LinearRegression().fit(X, Y)
         results = linmod.coef_
         return results
--- monte_carlo.py
+++ monte_carlo.py
@@ -1,8 +1,9 @@
 import numpy as np
+from random import choices as chc
 
-  def mc(self, *, n):
+  def mc_gen(self, *, n, datalength):
       '''
       feed dataset for bootstrapping draws, OR draws from param'd dist.
       '''
-
+      results = chc(np.arange(0, datalength), k=n)
       return results
