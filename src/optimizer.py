85716b76b4f9b2be2b9e392c07fc21c9d2a95d94
DesmondCole <desmond.r.cole@gmail.com>
2019-02-18 17:09:02 -0700

    updates

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,27 +1,22 @@
 import numpy as np
 
-def solve(X, T, q = 1):
+def solve(R, P, q = 1):
     '''
-    This runs a bellman.
-    X is an nd.array, of {nsims, states, periods}
-    T is an nd.array, of {states, states}
+    This runs a finite horizon backwards recursion to select the
+    optimal state matrix and max value.
+    R is an nd.array, of {sims, states, states, periods}
+    P is an nd.array, of {sims, states, states, periods}
     q indicates the shortest possible switching time. Default to 1 hour.
     '''
-    nsims, states, periods = X.shape
+    nsims, states, periods = R[:,:,0,:].shape
     optim_pol = np.zeros((nsims, periods))
     optim_val = np.zeros(nsims)
     for i in np.arange(0, nsims):
-        U = X[i, :, :]
-        V0 = np.zeros((states, periods))
-        V1 = U[:, -1]
+        V = np.zeros((states, periods))
         for j in np.arange(periods-2, -1, -1):
             for k in np.arange(0, states):
-                V0[k, j] = U[k, j] + max(T[:, k] + V1)
-            V1 = V0[:, j]
-        for j in np.arange(0, periods, 1):
-            optim_pol[i, j] = np.argmax(V0[:, j])
-        optim_val[i] = max(V1)
-    results = dict()
-    results["opt_val"] = optim_val
-    results["opt_pol"] = optim_pol
+                V[k,j] = max(R[i,k,:,j] + np.multiply(P[i,k,:,j], V[:,j+1]))
+            optim_pol[i,j] = np.argmax(V[:,j])
+        optim_val[i] = max(V[:,j])
+    results = {'opt_val': optim_val, 'opt_pol': optim_pol}
     return results
--- src/storage_opt.py
+++ src/storage_opt.py
@@ -7,5 +7,42 @@ class DispatchAsset:
     Feeds the optimizer storage characteristics, and runs it to generate values.
     '''
     def __call__(self, *, asset, gen, market):
-        opt_states = opt.solve(value_mat, trans_cost_mat)
+        reward_mat = self._rewardvals(asset = asset)
+        prob_mat = self._probvals(asset = asset, gen = gen, market = market)
+        opt_states = opt.solve(reward_mat, prob_mat)
         return opt_states
+
+    def _rewardvals(self, *, asset, market):
+        '''
+        use transition profile to generate reward matrix.
+        market is a dictionary of prices, sims, and generation.
+        '''
+        periods = 24
+        sims = market['sims']
+        prices = market['prices']
+        rawmat = np.zeros((sims, asset['mw_cap'], asset['mw_cap'], periods))
+        for i in np.arange(0, sims):
+            for j in np.arange(0, periods):
+                rawmat[i, :, :, j] = np.matrix([[0,0,0],[.5,0,0],[1,.5,0]])
+                rawmat[i, :, :, j] = np.tril(rawmat[i, :, :, j])
+                rawmat[i, :, :, j] = prices[i, j] * rawmat[i, :, :, j]
+        results = rawmat
+        return results
+
+    def _probvals(self, *, asset, gen, market):
+        '''
+        use asset, generation, and market characteristics to generate transition
+        probability matrix.
+        '''
+        periods = 24
+        sims = gen['sims']
+        results = np.zeros((sims, asset['mw_cap'], asset['mw_cap'], periods))
+        generation = gen['generation']
+        rawmat = np.zeros((sims, asset['mw_cap'], asset['mw_cap'], periods))
+        for i in np.arange(0, sims):
+            for j in np.arange(0, periods):
+                rawmat[i, :, :, j] = np.matrix([[0,0,0],[.5,0,0],[1,.5,0]])
+                rawmat[i, :, :, j] = np.tril(rawmat[i, :, :, j])
+                rawmat[i, :, :, j] = prices[i, j] * rawmat[i, :, :, j]
+        results = rawmat
+        return results
